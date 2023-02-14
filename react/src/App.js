a77371f69dd022a9db154f5fc1fccb157ea6bb74
Luke Harries <lharries@users.noreply.github.com>
2023-02-14 10:36:03 +0000

    apply prettier

--- react/src/App.js
+++ react/src/App.js
@@ -1,38 +1,34 @@
-import "./App.css";
-import posthog from "posthog-js";
-import { Popup } from "./Popup";
+import './App.css'
+import posthog from 'posthog-js'
+import { Popup } from './Popup'
 
 posthog.init(
-  // new
-  "phc_aIYpRzs2pU7hk93fqoTWtNezdeMUsNRCQFw7vnvDoOs",
-  { api_host: "https://app.posthog.com" }
-);
+    // new
+    'phc_aIYpRzs2pU7hk93fqoTWtNezdeMUsNRCQFw7vnvDoOs',
+    { api_host: 'https://app.posthog.com' }
+)
 
 function App() {
-  return (
-    <div className="App">
-      <img
-        src="https://posthog.com/brand/posthog-logo.svg"
-        className="App-logo"
-        alt="logo"
-      />
-      <header className="App-header">
-        <h1>PostHog Popups</h1>
-        <p>
-          Powered by{"  "}
-          <a
-            href="https://posthog.com/docs/integrate/client/js#feature-flag-payloads"
-            target="_blank"
-            data-attr="json-link"
-            rel="noopener noreferrer"
-          >
-            JSON Feature Flags
-          </a>
-        </p>
-      </header>
-      <Popup />
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src="https://posthog.com/brand/posthog-logo.svg" className="App-logo" alt="logo" />
+            <header className="App-header">
+                <h1>PostHog Popups</h1>
+                <p>
+                    Powered by{'  '}
+                    <a
+                        href="https://posthog.com/docs/integrate/client/js#feature-flag-payloads"
+                        target="_blank"
+                        data-attr="json-link"
+                        rel="noopener noreferrer"
+                    >
+                        JSON Feature Flags
+                    </a>
+                </p>
+            </header>
+            <Popup />
+        </div>
+    )
 }
 
-export default App;
+export default App
